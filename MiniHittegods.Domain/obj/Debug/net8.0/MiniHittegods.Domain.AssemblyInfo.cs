23e15bfdbaedf4b2706376fc7405d7d346383885
roan <hro102025@gmail.com>
2026-03-06 10:14:43 +0100

    :)

--- MiniHittegods.Domain/obj/Debug/net8.0/MiniHittegods.Domain.AssemblyInfo.cs
+++ MiniHittegods.Domain/obj/Debug/net8.0/MiniHittegods.Domain.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MiniHittegods.Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+980c54de0a2fdd5aa64030035e042586254800c8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+14b9fe5032398c67cc072cbf9811b498bcf28cf7")]
 [assembly: System.Reflection.AssemblyProductAttribute("MiniHittegods.Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MiniHittegods.Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
